a063e581844a4d2f7ccd75777ae472813ab11c26
maryamkhan50 <142137178+maryamkhan50@users.noreply.github.com>
2023-10-15 01:16:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+//all exersices of chapter 1 mixed.
+console.log("Hello world!");
+alert("javascript first assignment");
+// this is single line Comment
+console.log("the short key of commenting code is ctrl+/");
+/*let status = "new";
+let scared = true;
+if (status === "new") 
+console.log("Welcome to JavaScript!");
+if (scared) 
+console.log("Don't worry you will be fine!");
+this is multi line comment*/
+// this is prompt
+prompt("wish me best of luck!");
+// javascript is fun, check it out!
+
+
